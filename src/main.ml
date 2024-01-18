81d877d4947529ded874b45f77d57965cdb864d6
Thomas Leonard <talex5@gmail.com>
2024-01-18 16:03:16 +0000

    Add eio-trace run
    This records a trace to a temporary file and opens the UI while it's
    running.

--- src/main.ml
+++ src/main.ml
@@ -17,25 +17,37 @@ let child_args =
     
 let eio_trace_gtk = "eio-trace-gtk"
 
-let exec_gtk args =
+let find_eio_trace_gtk () =
   let self = Sys.argv.(0) in
-  let gtk_exe =
-    if Filename.is_implicit self then eio_trace_gtk
-    else (
-      (* Try to find the GTK binary in our own directory first.
-         Useful for testing without installing. *)
-      let my_dir = Filename.dirname self in
-      let path = Filename.concat my_dir eio_trace_gtk in
-      if Sys.file_exists path then path
-      else eio_trace_gtk
-    )
-  in
+  if Filename.is_implicit self then eio_trace_gtk
+  else (
+    (* Try to find the GTK binary in our own directory first.
+       Useful for testing without installing. *)
+    let my_dir = Filename.dirname self in
+    let path = Filename.concat my_dir eio_trace_gtk in
+    if Sys.file_exists path then path
+    else eio_trace_gtk
+  )
+
+let exec_gtk args =
+  let gtk_exe = find_eio_trace_gtk () in
   try
     Unix.execvp gtk_exe (Array.of_list (gtk_exe :: args))
   with Unix.Unix_error(ENOENT, _, _) ->
     Fmt.error "%S not found; can't run UI" gtk_exe
 
 let show tracefile = exec_gtk ["show"; tracefile]
+
+let run ~fs ~proc_mgr args =
+  let gtk_exe = find_eio_trace_gtk () in
+  let ui tracefile =
+    Eio.Process.run proc_mgr [gtk_exe; "show"; tracefile];
+    Ok ()
+  in
+  Record.run ~fs ~proc_mgr ~ui args
+
+let record ~fs ~proc_mgr tracefile args =
+  Record.run ~fs ~proc_mgr ~tracefile args
 
 let render tracefile output =
   if Filename.check_suffix output ".svg" then
@@ -49,8 +61,9 @@ let cmd env =
   let path x = Eio.Path.( / ) fs $$ x in
   Cmd.group (Cmd.info "eio-trace")
   @@ List.map (fun (name, term) -> Cmd.v (Cmd.info name) term) [
-    "record", Record.main ~fs ~proc_mgr      $$ path tracefile $ child_args;
+    "record", record ~fs ~proc_mgr           $$ path tracefile $ child_args;
     "dump",   Dump.main Format.std_formatter $$ path tracefile;
+    "run",    run ~fs ~proc_mgr              $$ child_args;
     "show",   show                           $$ tracefile;
     "render", render                         $$ tracefile $ imagefile;
   ]
--- src/record.mli
+++ src/record.mli
@@ -1,6 +1,7 @@
-val main :
+val run :
+  ?ui : (string -> (unit, string) result) ->
+  ?tracefile:_ Eio.Path.t ->
   proc_mgr:_ Eio.Process.mgr ->
   fs:_ Eio.Path.t ->
-  _ Eio.Path.t ->
   string list ->
   (unit, string) result
